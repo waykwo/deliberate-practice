ffc722873300aaea1805e9c84ab084e7a7cca800
greenman76 <72991933+waykwo@users.noreply.github.com>
2024-07-28 15:33:06 -0400

    Exercise 1 rb and py

---
+++ practice.py
@@ -0,0 +1,3 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+number = 6
+print(number + 10)
--- practice.rb
+++ practice.rb
@@ -4,3 +4,6 @@
 # git push origin main
 
 
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+number = 6
+puts number = number + 10
