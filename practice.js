5d8b900c21186e826f1501f211498ade9cff8937
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-01 20:46:52 -0400

    practice.js 01-variables1 q7 to q10

--- practice.js
+++ practice.js
@@ -14,8 +14,9 @@ let num2 = 6;
 console.log(num1 + num2);
 
 // 4. Write a program that uses a variable to store a word, then prints that word in reverse order.
-
-
+let word = "drawer";
+reverse = word.split("").reverse().join("");
+console.log(reverse)
 
 // 5. Write a program that uses a variable to store a number, then prints the number times 10.
 let number = 8;
@@ -29,3 +30,9 @@ console.log(word1.toUpperCase() + word2.toUpperCase())
 // 7. Write a program that uses a variable to store a word, then prints the number of letters in the word.
 let word = "Pneumonoultramicroscopicsilicovolcanoconiosis";
 console.log(word.length)
+
+// 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+let float = 3.14159265
+console.log(Math.round(float))
+
+// 9. Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
