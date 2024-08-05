886dd1ccbcfb678cbe80ee6dac3aefca3ae1cb15
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-05 19:20:18 -0400

    Deliberate practice 03-loops3-q1

--- practice.rb
+++ practice.rb
@@ -209,3 +209,17 @@ people.each do |person|
     ages.append(person[:age])
 end
 pp ages
+
+
+### 03-loops3.md
+
+# 1. Start with an array of numbers and create a new array with only the numbers less than 20.
+# For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+numbers = [2, 32, 80, 18, 12, 3]
+less_than_twenty = []
+numbers.each do |number|
+    if number < 20
+        less_than_twenty << number
+    end
+end
+pp less_than_twenty
--- scratch.rb
+++ scratch.rb
@@ -1,22 +1,10 @@
-# 2. Write a program to store the type of book (regular, reference, or special 
-# collection) and the number of days its overdue. Then calculate the fine 
-# amount based on the following conditions:
-#   If the book is a regular book and overdue by up to 7 days, the fine is $1 per day.
-#   If the book is a regular book and overdue by more than 7 days, the fine is $2 per day.
-#   If the book is a reference book, there is no fine, regardless of the number of days overdue.
-#   If the book is a special collection book, the fine is $5 per day, regardless of the number of days overdue.
-type = "special collection"
-days_overdue = 8
-if type == "reference"
-  fine = 0
-elsif type == "special collection"
-  fine = 5 * days_overdue
-else
-  if days_overdue <= 7
-    fine = days_overdue
-  else
-    fine = 2 * days_overdue
-  end
+# 1. Start with an array of numbers and create a new array with only the numbers less than 20.
+# For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+numbers = [2, 32, 80, 18, 12, 3]
+less_than_twenty = []
+numbers.each do |number|
+    if number < 20
+        less_than_twenty << number
+    end
 end
-
-puts fine
+pp less_than_twenty
