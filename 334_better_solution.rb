c1e1df83175dce09b23855fbd670cad7ba82800d
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-08 23:46:06 -0400

    Update 334_better_solution.rb

--- 334_better_solution.rb
+++ 334_better_solution.rb
@@ -1,3 +1,17 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Have the user enter 10 words, and allow for duplicate words. 
+# After the user is done, the program will tell the user 
+# which word was entered the most frequently.
+# For example, if the user enters: 
+#   apple, banana, orange, pear, apple, pear, apple, squash, apple, pear
+# The program will say: "apple was your most common word"
+# (That's because apple appeared in the user's list 4 times, more than any other word.)
+
+
 # Ask user for 10 words and store the words in an array
 words = []
 puts "You will be asked for ten words. Enter some duplicates and I'll tell you which words were entered most frequently."
