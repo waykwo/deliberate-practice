8215f693646d5a1753c6527421f46cc19cf46647
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-18 23:09:41 -0400

    Final 344 solution

---
+++ 344_better_solution.rb
@@ -0,0 +1,38 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Given an integer, write a method to return its roman numeral representation.
+# (1 is I, 2 is II, 3 is III, 4 is IV, 5 is V, 6 is VI, 7 is VII, etc.)
+# (You can see an example of all the rules here: https://byjus.com/maths/roman-numerals/#chart)
+
+values_and_symbols = {
+  1000 => "M",
+  900 => "CM",
+  500 => "D",
+  400 => "CD",
+  100 => "C",
+  90 => "XC",
+  50 => "L",
+  40 => "XL",
+  10 => "X",
+  9 => "IX",
+  5 => "V",
+  4 => "IV",
+  1 => "I"
+}
+
+puts "Please enter an integer:" 
+integer = gets.chomp.to_i
+roman_symbol = ""
+remainder = 0
+values_and_symbols.each do |key, value|
+  if integer >= key
+    multiple = (integer - integer.remainder(key)) / key
+    roman_symbol << value * multiple
+    remainder = integer.remainder(key)
+    integer = remainder
+  end
+end
+pp roman_symbol, remainder
