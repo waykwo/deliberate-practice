083da43ef1bf51a19122af05ca5fee4009d0493b
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-19 22:09:17 -0400

    Deliberate practice rb

--- scratch.rb
+++ scratch.rb
@@ -640,12 +640,33 @@
 
 # 8. Use a nested loop to find the largest sum of any two different numbers within an array.
 # For example, [1, 8, 3, 10] becomes 18.
-numbers = [1, 8, 3, 10]
-sums = []
-numbers.each do |number|
-  other_numbers = numbers.reject {|other_number| other_number == number}
-  other_numbers.each do |other|
-    sums << number + other
+# numbers = [1, 8, 3, 10]
+# sums = []
+# numbers.each do |number|
+#   other_numbers = numbers.reject {|other_number| other_number == number}
+#   other_numbers.each do |other|
+#     sums << number + other
+#   end
+# end
+# pp sums.max
+
+# 9. Use nested loops with an array of numbers to compute a new array containing 
+# the first two numbers (from the original array) that add up to the number 10. 
+# If there are no two numbers that add up to 10, return false.
+# For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+originals = [2, 5, 3, 1, 0, 7, 11]
+sum_10 = []
+originals.each do |original|
+  seconds = originals.reject {|number| number == original}
+  seconds.each do |second|
+    if original + second == 10 && sum_10.length < 2
+      sum_10 << original
+      sum_10 << second
+    end
   end
 end
-pp sums.max
+if sum_10.length == 2
+  pp sum_10
+else
+  puts false
+end
