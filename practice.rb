316b9e91b1423864ae5df6e24f3ab348122efd0d
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-27 11:19:16 -0400

    Deliberate practice rb

--- practice.rb
+++ practice.rb
@@ -629,8 +629,14 @@ pp array
 # the :id key in each of the array's hashes.
 # For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} 
 # becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
-
-
+people = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+people_array = Array.new
+people.each do |id, person|
+  person[:id] = id
+  people_array << person
+end
+pp people_array
+### This needs practice
 
 
 
--- scratch.rb
+++ scratch.rb
@@ -722,6 +722,14 @@
 # the :id key in each of the array's hashes.
 # For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} 
 # becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
-hash = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
-mod_hash = hash.select {|key, value| key = key + value}
-pp mod_hash
+people = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+people_array = []
+people.each do |id, person|
+  person[:id] = id
+  people_array << person
+end
+pp people_array
+
+
+new_array = Array.new
+pp new_array
