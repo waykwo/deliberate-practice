ceb76fcb87291a045659aab1465424513d73a93c
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-12 13:26:51 -0400

    deliberate practice js

--- practice.js
+++ practice.js
@@ -118,3 +118,24 @@ console.log(`I would like to visit ${city1}, ${city2}, and ${city3}.`)
 
 // === exercises/02-conditionals1.md
 
+// 1. Use a variable to store a number, then write a condition that prints 0 if 
+// the number is equal to 10, and prints -1 otherwise.
+let number = 10;
+if (number === 10) {
+    console.log(0);
+} else {
+    console.log(-1);
+};
+
+// 2. Use a variable to store a number, then write a condition that prints -1 
+// if the number is less than 10, prints 1 if the number is greater than 10, 
+// and prints 0 if the number is equal to 10.
+let number = 10;
+if (number < 10) {
+    console.log(-1);
+} else if (number > 10) {
+    console.log(1);
+} else {
+    console.log(0);
+};
+
