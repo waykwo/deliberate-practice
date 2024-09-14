f376b3c03438f6ee203c94292fd28b0076cfdf34
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-14 08:54:53 -0400

    deliberate practice js

--- practice.js
+++ practice.js
@@ -224,3 +224,36 @@ if (age <= 12) {
     price = 10
 };
 console.log(`Age: ${age}, Time: ${time}, Price: ${price}`)
+
+// 2. Write a program to store the type of book (regular, reference, or special 
+// collection) and the number of days its overdue. Then calculate the fine 
+// amount based on the following conditions:
+
+// If the book is a regular book and overdue by up to 7 days, the fine is $1 per day.
+// If the book is a regular book and overdue by more than 7 days, the fine is 
+//  $2 per day.
+// If the book is a reference book, there is no fine, regardless of the number 
+//  of days overdue.
+// If the book is a special collection book, the fine is $5 per day, regardless 
+//  of the number of days overdue.
+const bookTypes = ["regular", "reference", "collection"];
+const random = Math.floor(Math.random() * bookTypes.length);
+let bookType = bookTypes[random];
+console.log(`Book type: ${bookType}`);
+
+let overdueDays = Math.floor(getRandomArbitrary(1, 14));
+let fine;
+
+if (bookType === "regular" && overdueDays <= 7) {
+    fine = overdueDays;
+} else if (bookType === "regular" && overdueDays > 7) {
+    fine = overdueDays * 2;
+} else if (bookType === "reference") {
+    fine = 0;
+} else if (bookType === "collection") {
+    fine = overdueDays * 5;
+} else {
+    console.log("Error with conditions!")
+};
+
+console.log(`Overdue days: ${overdueDays}; Fine: ${fine}`)
