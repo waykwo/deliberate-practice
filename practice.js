375cec48bba5a8c1e7928a146e1a056c5d507788
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-14 08:20:10 -0400

    deliberate practice js

--- practice.js
+++ practice.js
@@ -201,3 +201,26 @@ if (num > 80) {
 } else {
     console.log(1);
 }
+
+
+// 02-conditionals3.md
+
+// 1. Write a program that stores a customer's age and a movie's time in two 
+// separate variables. Then calculate the price of a movie ticket based on the 
+// following conditions:
+
+// If the age is 12 years old or younger, the ticket price is $5.
+// If the age is between 13 and 59 years old and the movie is before 6 PM, the 
+//  ticket price is $7. After 6 PM, the ticket price is $10.
+// If the customer is 60 years old or older, the ticket price is $7.
+let age = Math.floor(getRandomArbitrary(4, 70));
+let time = Math.floor(getRandomArbitrary(4, 10));
+let price;
+if (age <= 12) {
+    price = 5
+} else if ((13 < age < 60 && time < 6) || age > 60) {
+    price = 7
+} else {
+    price = 10
+};
+console.log(`Age: ${age}, Time: ${time}, Price: ${price}`)
