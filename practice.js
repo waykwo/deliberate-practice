55e531cb1ac170ebb2911cc8d8fd31073f93e540
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-15 15:26:15 -0400

    Add more javascript exercies

--- practice.js
+++ practice.js
@@ -697,3 +697,8 @@ sports.forEach((sport) => {
   sportsString += sport;
 });
 console.log(sportsString);
+
+// 3. Start with an array of hashes and compute the sum of the prices (from the 
+// :price key).
+// For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, 
+// {name: "book", price: 4}] becomes 105.
