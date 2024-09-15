b00514465db50c8dd56b2bd163dbf4c298e0b338
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-15 14:22:44 -0400

    deliberate practice js

--- practice.js
+++ practice.js
@@ -589,3 +589,21 @@ strings.forEach((string) => {
   };
 });
 console.log(shortStrings);
+
+// 6. Start with an array of hashes and create a new array with only the hashes 
+// with names shorter than 6 letters (from the :name key).
+// For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, 
+// {name: "book", price: 4}] 
+// becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+let items = [
+  {name: "chair", price: 100},
+  {name: "pencil", price: 1}, 
+  {name: "book", price: 4}
+];
+let itemsShortNames = []
+  items.forEach((item) => {
+  if (item.name.length < 6) {
+    itemsShortNames.push(item);
+  };
+});
+console.log(itemsShortNames);
--- scratch.js
+++ scratch.js
@@ -1,8 +1,12 @@
-let strings = ["a", "man", "a", "plan", "a", "canal", "panama"];
-let shortStrings = [];
-strings.forEach((string) => {
-  if (string.length < 4) {
-    shortStrings.push(string);
+let items = [
+  {name: "chair", price: 100},
+  {name: "pencil", price: 1}, 
+  {name: "book", price: 4}
+];
+let itemsShortNames = []
+  items.forEach((item) => {
+  if (item.name.length < 6) {
+    itemsShortNames.push(item);
   };
 });
-console.log(shortStrings);
+console.log(itemsShortNames);
