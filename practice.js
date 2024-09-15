4a16e3e4a7c62ab9b4b52672bf2c61897817537c
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-15 15:31:01 -0400

    deliberate practice js

--- practice.js
+++ practice.js
@@ -702,3 +702,13 @@ console.log(sportsString);
 // :price key).
 // For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, 
 // {name: "book", price: 4}] becomes 105.
+let items = [
+  {name: "chair", price: 100},
+  {name: "pencil", price: 1}, 
+  {name: "book", price: 4}
+];
+let sum = 0;
+items.forEach((item) => {
+  sum += item.price;
+});
+console.log(sum);
--- scratch.js
+++ scratch.js
@@ -1,6 +1,10 @@
-let sports = ["volleyball", "basketball", "badminton"];
-let sportsString = ""
-sports.forEach((sport) => {
-  sportsString += sport;
+let items = [
+  {name: "chair", price: 100},
+  {name: "pencil", price: 1}, 
+  {name: "book", price: 4}
+];
+let sum = 0;
+items.forEach((item) => {
+  sum += item.price;
 });
-console.log(sportsString);
+console.log(sum);
