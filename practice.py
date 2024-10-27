bdf8b66b400c6f1dc15acecb36c0fe9e19d05321
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-27 13:13:15 -0400

    deliberate practice py

--- practice.py
+++ practice.py
@@ -1,19 +1,23 @@
 # 01-variables1
 
-# 1. Write a program that uses a variable to store a number, then prints that number plus 10.
+# 1. Write a program that uses a variable to store a number, then prints that 
+# number plus 10.
 number = 6
 print(number + 10)
 
-# 2. Write a program that uses a variable to store a word, then prints that word with all capital letters.
+# 2. Write a program that uses a variable to store a word, then prints that 
+# word with all capital letters.
 word = "hello"
 print(word.upper())
 
-# 3. Write a program that uses variables to store two numbers, then prints the numbers added together.
+# 3. Write a program that uses variables to store two numbers, then prints the 
+# numbers added together.
 num_1 = 3
 num_2 = 6
 print(num_1 + num_2)
 
-# 4. Write a program that uses a variable to store a word, then prints that word in reverse order.
+# 4. Write a program that uses a variable to store a word, then prints that 
+# word in reverse order.
 word = "hello"
 reversed = ""
 for i in range(1, len(word)):
@@ -28,32 +32,38 @@ print(reversed)
 word = "world"
 print(word[::-1])
 
-# 5. Write a program that uses a variable to store a number, then prints the number times 10.
+# 5. Write a program that uses a variable to store a number, then prints the 
+# number times 10.
 number = 8
 print(number * 10)
 
-# 6. Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+# 6. Write a program that uses variables to store two words, then prints both 
+# words on the same line in all capital letters.
 word_1 = "hello"
 word_2 = "world"
 print(word_1.upper() + word_2.upper())
 
-# 7. Write a program that uses a variable to store a word, then prints the number of letters in the word.
+# 7. Write a program that uses a variable to store a word, then prints the 
+# number of letters in the word.
 word = "Pneumonoultramicroscopicsilicovolcanoconiosis"
 print(len(word))
 
-# 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+# 8. Write a program that uses a variable to store a number with decimals, then 
+# prints the number as an integer.
 number = 3.14159265
 print(int(number))
 
 
 ### 01-variables2.md
 
-# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+# 2. Write a program that uses variables to store a first and last name, then 
+# prints the full name in one line using string interpolation (the #{} operator).
 first = "Sherlock"
 last = "Holmes"
 print(f"{first} {last}")
 
-# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+# 3. Write a program that asks the user to input a word. If the word is "marco", 
+# print "polo".
 word = input("Please enter a word: ")
 if word == "marco":
     print("polo")
@@ -72,7 +82,8 @@ colour2 = "green"
 colour3 = "blue"
 print(f"The colours are: {colour1}, {colour2}, and {colour3}.")
 
-# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+# 6. Write a program that asks the user to enter a name. If the name is not 
+# "Santa", print "You're not Santa."
 name = input("What is your name?: ")
 if name != "Santa":
     print("You're not Santa.")
--- scratch.js
+++ scratch.js
@@ -1,6 +1,15 @@
-let hash = { a: 1, b: 2, c: 3, d: 4 };
-let array = [];
-Object.entries(hash).forEach(([key, value]) => {
-  array.push(key, value);
+let priceObject = { chair: 75, book: 15 };
+let items = [
+  { name: "chair", color: "red", weight: 10 },
+  { name: "book", color: "black", weight: 1 },
+];
+let combined = {};
+items.forEach(item => {
+
+  combined[item.name] = {
+    price: priceObject[item.name],
+    color: item.color,
+    weight: item.weight
+  };
 });
-console.log(array);
+console.log(combined);
