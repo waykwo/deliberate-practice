6f8ad5a7c448da54576520dc2436f40add6c0bdd
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-27 12:50:53 -0400

    deliberate practice js

--- practice.js
+++ practice.js
@@ -1230,3 +1230,27 @@ Object.entries(hash).forEach(([key, value]) => {
   array.push(key, value);
 });
 console.log(array);
+
+// 8. Combine data from a hash with names and prices and an array of hashes 
+// with names, colors, and weights to make a new hash.
+// For example,
+// {"chair" => 75, "book" => 15} and 
+// [{name: "chair", color: "red", weight: 10},
+// {name: "book", color: "black", weight: 1}] becomes 
+// {"chair" => {price: 75, color: "red", weight: 10}, 
+// "book" => {price: 15, color: "black", weight: 1}}.
+let priceObject = { chair: 75, book: 15 };
+let items = [
+  { name: "chair", color: "red", weight: 10 },
+  { name: "book", color: "black", weight: 1 },
+];
+let combined = {};
+items.forEach(item => {
+
+  combined[item.name] = {
+    price: priceObject[item.name],
+    color: item.color,
+    weight: item.weight
+  };
+});
+console.log(combined);
