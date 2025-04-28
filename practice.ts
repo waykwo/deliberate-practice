6c1dfcb06fbcf1fef66ef4b71bdb03d01b5ccf93
waykwo <72991933+waykwo@users.noreply.github.com>
2025-04-28 08:09:38 -0400

    practice.ts

---
+++ practice.ts
@@ -0,0 +1,6 @@
+// 01-varables2.md
+
+// 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+let first_name: string = "Jeff";
+let last_name: string = "Smith";
+console.log(first_name + " " + last_name);
